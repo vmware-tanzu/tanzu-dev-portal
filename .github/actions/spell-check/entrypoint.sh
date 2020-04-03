f5e87b6288a7c2b4b2532b9b474052c10863ee2a
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2020-04-03 16:04:49 -0400

    Add Spell Checking (#10)

---
+++ .github/actions/spell-check/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#set -e
+#set -x
+set -o pipefail
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+cd $GITHUB_WORKSPACE
+echo "--> Install spellchecker"
+npm install --global spellchecker-cli retext-syntax-urls retext-indefinite-article retext-repeated-words
+
+echo "--> spellcheck markdown files"
+spellchecker -f 'content/**/*.md' -d custom_dict.txt --plugins spell syntax-urls indefinite-article repeated-words
