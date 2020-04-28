d883b9b960a594e297c6a3568ba1ba7da051c2fe
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2020-04-27 20:06:30 -0700

    Add frontmatter plugin to spellchecker
    Skip spellchecker for frontmatter but still check description and title.

--- .github/actions/spell-check/entrypoint.sh
+++ .github/actions/spell-check/entrypoint.sh
@@ -19,4 +19,4 @@ echo "--> Install spellchecker"
 npm install --global spellchecker-cli retext-syntax-urls retext-indefinite-article retext-repeated-words
 
 echo "--> spellcheck markdown files"
-spellchecker -q --no-suggestions -l en-US -f 'content/**/*.md' -d custom_dict.txt --plugins spell syntax-urls indefinite-article repeated-words
+spellchecker -q --no-suggestions -l en-US -f 'content/**/*.md' --frontmatter-keys description -d custom_dict.txt --plugins frontmatter spell syntax-urls indefinite-article repeated-words
