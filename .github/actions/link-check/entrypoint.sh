0eda543af74c6d7b7ad175dd0ba7c386b92725f4
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2020-04-29 18:00:29 -0700

    Forget env var checks for now

--- .github/actions/link-check/entrypoint.sh
+++ .github/actions/link-check/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [[ -z "${HUGO_OPTIONS}" ]]; then
-  echo "Set the HUGO_OPTIONS env variable."
-  exit 1
-fi
-
-if [[ -z "${MUFFET_OPTIONS}" ]]; then
-  echo "Set the MUFFET_OPTIONS env variable."
-  exit 1
-fi
-
 LINE='##############################'
 MSGS='####### start action #########'
 
