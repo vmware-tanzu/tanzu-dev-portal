f01ff67500aa184bb63b72f70b39eba62501736e
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2020-05-01 08:54:48 -0700

    Minor output fix for link check script

--- .github/actions/link-check/entrypoint.sh
+++ .github/actions/link-check/entrypoint.sh
@@ -5,8 +5,8 @@ MAX_WAIT_TIME=600 # 5 min
 
 echo "--> Start hugo server"
 eval hugo server "${HUGO_OPTIONS}" > /dev/null &
+echo -n "--> Wait for hugo server to start..."
 for i in $(seq 0 ${MAX_WAIT_TIME}); do # 5 min
-    echo -n "--> Wait for hugo server to start..."
     sleep 0.5
     IS_SERVER_RUNNING=$(curl -LI ${LOCAL_HOST} -o /dev/null -w '%{http_code}' -s)
     if [[ "${IS_SERVER_RUNNING}" == "200" ]]; then
