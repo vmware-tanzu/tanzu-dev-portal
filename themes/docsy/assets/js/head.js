dd890b1093fef16043292152757fc35b56c0b111
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2020-06-26 08:27:03 -0700

    Move all head JS into separate script

--- themes/docsy/assets/js/head.js
+++ themes/docsy/assets/js/head.js
@@ -31,3 +31,21 @@ function isTvShowLive(showName) {
 
       return isLive;
 }
+
+var savedTheme = localStorage.getItem("light-dark-mode-storage") || "dark";
+changeTheme(savedTheme);
+function changeTheme(mode) {
+    if (mode === "light") {
+      var head = document.head;
+      var link = document.createElement("link");
+
+      link.type = "text/css";
+      link.rel = "stylesheet";
+      link.href = {{ printf "%s" "/css/light-theme.css" | relURL }};
+      link.id = "light-theme"
+
+      head.prepend(link);
+      
+      document.documentElement.classList.add('light-mode')
+    } 
+}
