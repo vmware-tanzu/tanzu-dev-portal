6c6694671e96f9ba64a1f7111e6d92067f9eb9fd
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2021-03-08 13:20:27 -0500

    refactor

--- themes/docsy/assets/js/base.js
+++ themes/docsy/assets/js/base.js
@@ -340,23 +340,7 @@ limitations under the License.
       }
     });
 
-    function getCookie(name) {
-      var dc = document.cookie;
-      var prefix = name + "=";
-      var begin = dc.indexOf("; " + prefix);
-      if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
-      } else {
-        begin += 2;
-        var end = document.cookie.indexOf(";", begin);
-        if (end == -1) {
-          end = dc.length;
-        }
-      }
 
-      return decodeURI(dc.substring(begin + prefix.length, end));
-    }
   });
 
   //Header search
--- themes/docsy/assets/js/head.js
+++ themes/docsy/assets/js/head.js
@@ -80,3 +80,21 @@ function sendAmplitudeUserProps(userProperties) {
 function setAmplitudeUserId(userId) {
   dataLayer.push({'event': 'setUserId', 'userId': userId});
 }
+
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+    begin = dc.indexOf(prefix);
+    if (begin != 0) return null;
+  } else {
+    begin += 2;
+    var end = document.cookie.indexOf(";", begin);
+    if (end == -1) {
+      end = dc.length;
+    }
+  }
+
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
