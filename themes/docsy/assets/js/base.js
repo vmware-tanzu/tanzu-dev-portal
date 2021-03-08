9ebf0cf966b5fab7ba56361532007659d5582979
Jesse Martinez <jessemartinezdesign@gmail.com>
2021-03-08 00:13:45 -0800

    Add hover nav menu

--- themes/docsy/assets/js/base.js
+++ themes/docsy/assets/js/base.js
@@ -311,4 +311,26 @@ limitations under the License.
         $('#mega-menus').toggleClass('no-border');
     });
 
+    //Nav hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {$dropdown.hover(
+        function() {
+            const $this = $(this);
+            $this.addClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "true");
+            $this.find($dropdownMenu).addClass(showClass);
+        },
+        function() {
+            const $this = $(this);
+            $this.removeClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "false");
+            $this.find($dropdownMenu).removeClass(showClass);
+        }
+        );
+    });
+
 }(jQuery));
