00ae6db01012e3932ec9d933d2ca4232208d5106
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2021-04-27 15:02:55 -0400

    add logging back in

--- netlify/functions/workshop-status.js
+++ netlify/functions/workshop-status.js
@@ -54,6 +54,7 @@ exports.handler = Sentry.AWSLambda.wrapHandler(async (event) => {
   const { body } = event;
   const jsonbody = JSON.parse(body);
   const workshopEvent = jsonbody.event;
+  console.log(jsonbody)
   if (eventTypes.includes(workshopEvent.name)) {
     const time = new Date(workshopEvent.timestamp);
     const eventProperties = {
