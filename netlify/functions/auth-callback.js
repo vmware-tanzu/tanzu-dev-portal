75443f96aacfda7d5fc3e597633cb63fa030ac69
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2021-04-28 06:04:54 -0400

    fix callback

--- netlify/functions/auth-callback.js
+++ netlify/functions/auth-callback.js
@@ -86,10 +86,16 @@ exports.handler = Sentry.AWSLambda.wrapHandler(async (event) => {
             },
         };
         const oneTrustCookieParsed = querystring.parse(cookies.OptanonConsent);
-        const groupposition = oneTrustCookieParsed.groups.search('C0002:') + 6;
-        console.log(oneTrustCookieParsed.groups[groupposition]);
-        if (oneTrustCookieParsed.groups[groupposition] === '0') {
-            jwtToken.id = randomToken();
+        if (oneTrustCookieParsed){
+            const groupposition = oneTrustCookieParsed.groups.search('C0002:') + 6;
+            if (oneTrustCookieParsed.groups[groupposition] === '0') {
+                jwtToken.id = randomToken();
+            }else {
+                jwtToken.name = decoded.payload.username;
+                jwtToken.id = decoded.payload.username;
+                jwtToken.acct = decoded.payload.acct;
+                jwtToken.sub = decoded.payload.sub;
+            }
         } else {
             jwtToken.name = decoded.payload.username;
             jwtToken.id = decoded.payload.username;
