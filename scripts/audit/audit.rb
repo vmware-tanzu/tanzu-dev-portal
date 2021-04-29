b0a21bded4dbe54cd662b05c0c5931dd9ee30c33
Brian McClain <brianmmcclain@gmail.com>
2021-04-29 10:10:15 -0400

    Add practices to audit script (#772)

--- scripts/audit/audit.rb
+++ scripts/audit/audit.rb
@@ -65,7 +65,7 @@ csv = CSV.open(options[:output], "w")
 
 # Gather the list of files to parse
 contentPath = File.join(options[:source], "/content/")
-contentFiles = Dir.glob(File.join(contentPath, "/blog/*.md")) + Dir.glob(File.join(contentPath, "/guides/**/*.md"))
+contentFiles = Dir.glob(File.join(contentPath, "/blog/*.md")) + Dir.glob(File.join(contentPath, "/guides/**/*.md")) + Dir.glob(File.join(contentPath, "/practices/**/*.md"))
 
 # Prepare the markdown renderer 
 markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
