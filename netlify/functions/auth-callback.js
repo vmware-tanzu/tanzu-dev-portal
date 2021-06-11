6f336921804752075fe8587978ecff750532f39c
Brian McClain <brianmmcclain@gmail.com>
2021-06-11 16:49:48 -0400

    Unminify redirect HTML

--- netlify/functions/auth-callback.js
+++ netlify/functions/auth-callback.js
@@ -121,7 +121,8 @@ exports.handler = Sentry.AWSLambda.wrapHandler(async (event) => {
             ? `${getSiteURL()}${parsed.path}?src=${parsed.referer}`
             : `${getSiteURL()}${parsed.path || ''}`;
 
-        const redirectBody = `<html><head><script>function getCookie(e){var t=document.cookie,n=e+"=",o=t.indexOf("; "+n);if(-1==o){if(0!=(o=t.indexOf(n)))return null}else{o+=2;var i=document.cookie.indexOf(";",o);-1==i&&(i=t.length)}return decodeURI(t.substring(o+n.length,i))}function setGTM(e,t,n,o,i){e[o]=e[o]||[],e[o].push({"gtm.start":(new Date).getTime(),event:"gtm.js"});var r=t.getElementsByTagName(n)[0],a=t.createElement(n),s="dataLayer"!=o?"&l="+o:"";a.async=!0,a.src="https://www.googletagmanager.com/gtm.js?id="+i+s,r.parentNode.insertBefore(a,r)}var timer;function waitForOnetrustActiveGroups(){document.cookie.indexOf("OptanonConsent")>-1&&document.cookie.indexOf("groups=")>-1?(clearTimeout(timer),setGTM(window,document,"script","dataLayer","GTM-TQ9H33K")):timer=setTimeout(waitForOnetrustActiveGroups,250)}document.cookie.indexOf("OptanonConsent")>-1&&document.cookie.indexOf("groups=")>-1?setGTM(window,document,"script","dataLayer","GTM-TQ9H33K"):waitForOnetrustActiveGroups(),dataLayer.push({event:"setUserId",userId:JSON.parse(atob(getCookie("nf_jwt").split(".")[1])).id});</script><title>Redirect</title><meta http-equiv="refresh" content="0;url=${redirect}" /></head><body></body></html>`;
+        var redirectBody = redirectTemplate.replace("REDIRECT_URL", redirect)
+        
         return {
             statusCode: 200,
             headers: {
@@ -140,3 +141,45 @@ exports.handler = Sentry.AWSLambda.wrapHandler(async (event) => {
 });
 
 
+var redirectTemplate = `<html>
+<head>
+    <script>
+        console.log("COOKIES:");
+        console.log(document.cookie);
+        function getCookie(e) {
+            var t = document.cookie,
+                n = e + "=",
+                o = t.indexOf("; " + n);
+            if (-1 == o) {
+                if (0 != (o = t.indexOf(n))) return null;
+            } else {
+                o += 2;
+                var i = document.cookie.indexOf(";", o);
+                -1 == i && (i = t.length);
+            }
+            return decodeURI(t.substring(o + n.length, i));
+        }
+        function setGTM(e, t, n, o, i) {
+            (e[o] = e[o] || []), e[o].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+            var r = t.getElementsByTagName(n)[0],
+                a = t.createElement(n),
+                s = "dataLayer" != o ? "&l=" + o : "";
+            (a.async = !0), (a.src = "https://www.googletagmanager.com/gtm.js?id=" + i + s), r.parentNode.insertBefore(a, r);
+        }
+        var timer;
+        function waitForOnetrustActiveGroups() {
+            document.cookie.indexOf("OptanonConsent") > -1 && document.cookie.indexOf("groups=") > -1
+                ? (clearTimeout(timer), setGTM(window, document, "script", "dataLayer", "GTM-TQ9H33K"))
+                : (timer = setTimeout(waitForOnetrustActiveGroups, 250));
+        }
+        console.log("NF_JWT TOKEN:");
+        console.log(getCookie("nf_jwt"));
+        document.cookie.indexOf("OptanonConsent") > -1 && document.cookie.indexOf("groups=") > -1 ? setGTM(window, document, "script", "dataLayer", "GTM-TQ9H33K") : waitForOnetrustActiveGroups(),
+            dataLayer.push({ event: "setUserId", userId: JSON.parse(atob(getCookie("nf_jwt").split(".")[1])).id });
+    </script>
+    <title>Redirect</title>
+    <meta http-equiv="refresh" content="0;url=REDIRECT_URL" />
+</head>
+<body></body>
+</html>
+'=`
