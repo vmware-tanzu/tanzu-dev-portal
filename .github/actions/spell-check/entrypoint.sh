af557e7925026abedd9df54bde04080a239e9ce2
Bryan Friedman <bryanfriedman@users.noreply.github.com>
2021-09-13 08:40:29 -0700

    Fix ignore strings for spell check

--- .github/actions/spell-check/entrypoint.sh
+++ .github/actions/spell-check/entrypoint.sh
@@ -20,5 +20,5 @@ npm install --global spellchecker-cli retext-syntax-urls retext-indefinite-artic
 
 echo "--> spellcheck markdown files"
 IGNORE_REGEX="[0-9a-f]{7} (?=.*[0-9])[0-9A-Za-z_\-]{11}" # GitHub hashes and YouTube IDs
-IGNORE_FILES="!(content/tv/tgik/*.md) !(content/tv/talk/*.md)" # Don't worry about TGIK/Tanzu Talk episodes for now as they scrape from YT
+IGNORE_FILES="!(content/tv/tgik/**/index.md) !(content/tv/talk/**/index.md)" # Don't worry about TGIK/Tanzu Talk episodes for now as they scrape from YT
 spellchecker -q --no-suggestions -l en-US -f 'content/**/*.md' $IGNORE_FILES -i $IGNORE_REGEX --frontmatter-keys title Title description Description -d custom_dict.txt --plugins frontmatter spell syntax-urls indefinite-article repeated-words
