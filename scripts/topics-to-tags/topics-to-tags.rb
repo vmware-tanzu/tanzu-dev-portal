2cd33fce53d04bbe9e72fc3e6f5bf88511a26526
Brian McClain <brianmmcclain@gmail.com>
2021-09-22 13:03:40 -0400

    Add script to merge topics into tags

---
+++ scripts/topics-to-tags/topics-to-tags.rb
@@ -0,0 +1,61 @@
+require 'optparse'
+require 'yaml'
+
+# Parse the command line flags
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: add-alias.rb [options]"
+
+    opts.on("-s", "--source PATH", "Path to local TDC Source") do |s|
+        options[:source] = s
+    end
+
+    opts.on("-d", "--dryrun", "Path to write the raw results") do |d|
+        options[:dry] = d
+    end
+end.parse!
+
+# Gather the list of files to parse
+contentPath = Dir.glob(File.join(File.join(options[:source], "/content/guides/**/*.md")))
+
+# Add alias
+contentPath.each do |f|
+    fData = File.open(f).read
+    contentMetadata = YAML.load(fData)
+
+    newTags = contentMetadata["tags"].nil? ? [] : contentMetadata["tags"]
+    topics = contentMetadata["topics"].nil? ? [] : contentMetadata["topics"]
+    newTags.append(contentMetadata["topics"])
+    newTags = newTags.flatten.uniq.compact
+    newTags = newTags.nil? ? [] : newTags
+    puts newTags
+    contentMetadata["tags"] = newTags
+    contentMetadata.delete("topics")
+    puts contentMetadata
+
+    # Most frontmatter should start with "---", meaning we split on the second occurance.
+    # However we should double check to make sure
+    splitFirst = false
+    if not fData.start_with? "---"
+        splitFirst = true
+    end
+
+    sContent = fData.split("---")
+    frontMatter, articleContent = ""
+
+    frontMatter = YAML.dump(contentMetadata)
+
+    if splitFirst
+        articleContent = sContent[1..]
+    else
+        articleContent = sContent[2..].join("---")
+    end
+
+    if not options[:dry]
+        outFile = f
+        puts "Writing to #{outFile} . . ."
+        File.open(outFile, "w") { |f|
+            f.write "#{frontMatter}---#{articleContent}"
+        }
+    end
+end
