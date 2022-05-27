dd7b1c4e4ff9c8e327629a55bd4e568a176c9437
Brian McClain <brianmmcclain@gmail.com>
2022-05-27 15:41:56 -0400

    Expand out logic to determine date

--- .github/actions/youtube-episodes/src/main.py
+++ .github/actions/youtube-episodes/src/main.py
@@ -77,7 +77,6 @@ def getNewEpisodesInPlaylist(playlistId, above, existingVideoIDs):
     for item in list.items:
         print("----> Processing metadata for video " + item.snippet.resourceId.videoId)
         videoId = item.snippet.resourceId.videoId
-        print("Video ID: " + videoId)
         response = requests.get(YT_API_BASE_URL + YT_API_QS + "&id=" + videoId)
         videoList = json.loads(response.text)
         videoInfo = {}
@@ -85,14 +84,18 @@ def getNewEpisodesInPlaylist(playlistId, above, existingVideoIDs):
             videoInfo = videoList["items"][0]["snippet"]
         except:
             continue
-        print(videoInfo)
         liveInfo = {}
         try:
             liveInfo = videoList["items"][0]["liveStreamingDetails"]
         except:
             liveInfo["scheduledStartTime"] = None
-        print(liveInfo)
-        episodeDate = fuzzyDate(liveInfo["scheduledStartTime"]) or fuzzyDate(videoInfo["publishedAt"]) or datetime.time()
+
+        if liveInfo.has_key("scheduledStartTime"):
+            episodeDate = fuzzyDate(liveInfo["scheduledStartTime"])
+        elif videoInfo.has_key("publishedAt"):
+            episodeDate = fuzzyDate(videoInfo["publishedAt"])
+        else:
+            episodeDate = datetime.time()
 
         video = {
             "videoId": videoId,
