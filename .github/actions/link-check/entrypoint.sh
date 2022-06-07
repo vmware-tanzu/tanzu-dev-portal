deb6ff9458aaa447dafbfcb6b22d975557931e73
Brian McClain <brianmmcclain@gmail.com>
2022-06-07 14:21:00 -0400

    Clean up script

--- .github/actions/link-check/entrypoint.sh
+++ .github/actions/link-check/entrypoint.sh
@@ -3,17 +3,15 @@
 LOCAL_HOST="http://localhost:1313/developer"
 MAX_WAIT_TIME=600 # 5 min
 
+# Provide default options if none are provided
 HUGO_OPTIONS="${HUGO_OPTIONS:=-b http://localhost:1313/developer}"
 MUFFET_OPTIONS="${MUFFET_OPTIONS:=-t 30 -e 'https?' --exclude='/developer/get-workshop'}"
-
-echo "HUGO_OPTIONS=$HUGO_OPTIONS"
-echo "MUFFET_OPTIONS=$MUFFET_OPTIONS"
 
 # Ensure that the git log can be pulled
 git config --global --add safe.directory /github/workspace
 
 echo "--> Start hugo server"
-eval hugo server "${HUGO_OPTIONS}" &
+eval hugo server "${HUGO_OPTIONS}" > /dev/null &
 echo -n "--> Wait for hugo server to start..."
 for i in $(seq 0 ${MAX_WAIT_TIME}); do # 5 min
     sleep 0.5
