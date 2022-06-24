74476eac89eaf959b93c9eb687da9667ef239250
Brian McClain <brianmmcclain@gmail.com>
2022-06-24 13:40:37 -0400

    Add GH Action to process bio template pages (#1913)

---
+++ .github/actions/bio-template/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#set -e
+#set -x
+set -o pipefail
+
+if [[ -z "$GITHUB_WORKSPACE" ]]; then
+  echo "Set the GITHUB_WORKSPACE env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+echo "--> Run the audit"
+cd $GITHUB_WORKSPACE
+ruby .github/actions/bio-template/src/bio-template.rb --source .
