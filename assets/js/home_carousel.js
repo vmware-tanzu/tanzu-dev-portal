15380e3007e96d56fbd6db5a28fb92e3f9571dda
Daniel Ramirez <zoerunner@gmail.com>
2023-05-15 08:57:11 -0700

    Update carousel name. Fix center positioning for when users resize or move page

--- assets/js/home_carousel.js
+++ assets/js/home_carousel.js
@@ -1,11 +1,11 @@
 function autoScroll(element) {
     let scrollSpeed = 0;
     const maxSpeed = 10;
-    const center = {
-        x: element.offsetLeft + element.offsetWidth / 2,
-    };
 
     function handleMouseMove(event) {
+        const center = {
+            x: element.offsetLeft + element.offsetWidth / 2,
+        };
         const distanceFromCenterX = Math.abs(event.clientX - center.x);
         const distanceFromEdgeX = Math.max(distanceFromCenterX - 100, 0);
         const scrollX = (event.clientX > center.x ? 1 : -1) * (distanceFromEdgeX / (element.offsetWidth / 2)) * maxSpeed;
